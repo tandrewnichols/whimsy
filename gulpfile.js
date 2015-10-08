7fd3215eada6f2531700901ec759e95780d3a827
Andrew Nichols <tandrewnichols@gmail.com>
2015-10-08 10:00:21 -0400

    Add jshint and ci tasks and point travis at the ci task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var jshint = require('gulp-jshint');
 var paths = {
   tests: 'test/**/*.coffee',
   lib: ['lib/**/*.js', 'bin/**/*.js']
@@ -14,6 +15,21 @@ gulp.task('test', function() {
     }));
 });
 
+gulp.task('lint', function() {
+  return gulp.src(paths.lib)
+    .pipe(jshint({
+      lookup: false,
+      eqeqeq: true,
+      es3: true,
+      indent: 2,
+      newcap: true,
+      quotmark: 'single',
+      boss: true
+    })) .pipe(jshint.reporter('jshint-stylish'));
+});
+
 gulp.task('watch', function() {
   gulp.watch(paths.lib.concat(paths.tests), ['test']);
 });
+
+gulp.task('ci', ['lint', 'test']);

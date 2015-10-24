67c80b6c9c704b63c9ba6fdde0083ba8541ce8ef
Andrew Nichols <tandrewnichols@gmail.com>
2015-10-23 21:56:13 -0400

    Fix codeclimate reporting

--- gulp/codeclimate.js
+++ gulp/codeclimate.js
@@ -5,7 +5,9 @@ var codeclimate = require('gulp-codeclimate-reporter');
 gulp.task('codeclimate', ['test'], function(cb) {
   if (process.version.indexOf('v4') > -1) {
     gulp.src('coverage/lcov.info', { read: false })
-      .pipe(codeclimate());
+      .pipe(codeclimate({
+        token: 'c8d9d6be730725a6b7a02073283353c5756640ef71753a06bb96c77c7447266f'
+      }));
   }
 });
 

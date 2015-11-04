2ff4606ae32445423cca9cff073f58c11e869938
Andrew Nichols <tandrewnichols@gmail.com>
2015-11-03 22:08:31 -0500

    Sequence the test task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 var sequence = require('gulp-sequence');
 require('file-manifest').generate('./gulp', ['**/*.js', '!config.js']);
 gulp.task('travis', sequence(['lint', 'cover', 'int', 'phantom'], 'codeclimate'));
-gulp.task('test', ['cover', 'int', 'browser']);
+gulp.task('test', sequence('cover', 'int', 'browser'));
 gulp.task('default', ['lint', 'test']);
 gulp.task('build', sequence('clean:dist', 'browserify', 'uglify'));
